fb60cb104c8fd877a59e8563e75fe797741bd2df
Shibu Manoharan <shibumanoharan@Shibus-Mac-mini.local>
2021-04-03 12:52:30 +0100

    practise

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Online Test
       </p>
